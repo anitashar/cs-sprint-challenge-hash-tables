91e6364b984093ccc617ddc8b5f7e254b0889f57
anitashar <anitashar2006@gmail.com>
2020-09-11 20:48:31 -0700

    done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,19 +1,5 @@
 # #  Hint:  You may not need all of these.  Remove the unused functions.
-# class Ticket:
-#     def __init__(self, source, destination):
-#         self.source = source
-#         self.destination = destination
 
-
-# def reconstruct_trip(tickets, length):
-#     """
-#     YOUR CODE HERE
-#     """
-#     
-
-#     return route
-
-#  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
